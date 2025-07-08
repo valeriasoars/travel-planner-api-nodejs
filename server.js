00c7312e20e1ffb14498b72cef998db3950bb832
Valeria Soares <valeriasoaressantos2@gmail.com>
2025-07-07 21:15:23 -0300

    ..

--- server.js
+++ server.js
@@ -16,14 +16,7 @@ connectDB()
 
 
 const app = express()
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-app.options('*', cors());
-
+app.use(cors())
 app.use(express.json())
 
 app.use("/api/user", userRoutes)
